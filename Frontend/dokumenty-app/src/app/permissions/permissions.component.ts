0f544e7c1d6ecb0c9c241a3ab5fad235a2d5a637
Maaateusz <alamakotaisi@gmail.com>
2019-06-01 20:03:01 +0200

    uprawnienia

--- Frontend/dokumenty-app/src/app/permissions/permissions.component.ts
+++ Frontend/dokumenty-app/src/app/permissions/permissions.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-permissions',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PermissionsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getAllUsers();
   }
 
+  serverPath = "http://localhost:8080";
+
+  users: User[] = [{ id: 1, email: "qwe", role: "USER" },
+  { id: 2, email: "asd", role: "USER" },
+  { id: 3, email: "zxc", role: "ADMIN" }];
+
+  dodajUprawnienie(user: User) {
+    console.log(JSON.stringify(user)); //w JSON
+    user.role = "ADMIN";
+    let url = "/user/asd";
+    this.http.post(this.serverPath + url, user).subscribe(
+      isValid => {
+        //window.location.reload();
+        console.log(JSON.stringify(user));
+      },
+      err => { alert("Error: server not responding"); }
+    );
+  }
+
+  getAllUsers() {
+    let url = "user/all";
+    this.http.get<User[]>(this.serverPath + url).subscribe(
+      res => {
+        this.users = res;
+      },
+      err => {
+        alert("Error: server not responding!")
+      }
+    );
+  }
 }
---
+++ Frontend/dokumenty-app/src/app/user.ts
@@ -0,0 +1,5 @@
+export class User {
+    id:Number;
+    email:String;
+    role:String;
+}
